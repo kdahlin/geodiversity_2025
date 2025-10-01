23d2f000ea5a3fc13e3fe1ddae38bfc1915030ca
mdrutgers <rutgers1@msu.edu>
2025-10-01 10:30:44 -0400

    Update mdr_1001_Viz.R

--- sandbox/mdr_1001_Viz.R
+++ sandbox/mdr_1001_Viz.R
@@ -1,1 +1,13 @@
 library(terra)
+
+#get path to mosaic data
+
+mosaic.dem <- "C:\\Users\\rutgers1\\Documents\\GitHub\\geodiversity_2025\\processed_tifs\\ORNL_2018_DEM_mosaic_20250925.tif"
+r1 <- rast(mosaic.dem)
+plot(r1)
+
+metrics <- terrain(r1)
+plot(metrics)
+
+aspect <- terrain(r1, v = "aspect")
+plot(aspect)
