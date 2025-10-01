823db3e633584016541bf4f9cc6e513526e16caa
laurache111 <rachel.lauyusan@gmail.com>
2025-10-01 10:39:58 -0400

    Create RL_1001_Viz.R

---
+++ sandbox/RL_1001_Viz.R
@@ -0,0 +1,30 @@
+#install packages
+library(terra)
+library(geodiv)
+library(raster)
+library(mapdata)
+library(ggplot2)
+library(cowplot)
+library(sf)
+library(rasterVis)
+
+setwd("C://Users/rache/Documents/geodiversity_2025/geodiversity_2025/sandbox/")
+
+mosaic <- "C://Users/rache/Documents/geodiversity_2025/geodiversity_2025/processed_tifs/ORNL_2018_DEM_mosaic_20250925.tif"
+
+r1<-rast(mosaic)
+plot(r1)
+
+metrics_slope <- terrain(r1, v="slope")
+metrics_aspect <- terrain(r1, v="aspect")
+plot(metrics_slope)
+plot(metrics_aspect)
+
+###Calulate global surface gradient metrics
+#Average roughness
+roughness <- sa(r1)
+roughness
+
+#Surface bearing index
+surfacebearing <- sbi(r1)
+surfacebearing
