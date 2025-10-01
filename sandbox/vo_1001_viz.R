0dfce0a606090674722bc73fdb6b2561cb0271dc
rosscour <rosscour@msu.edu>
2025-10-01 10:41:52 -0400

    Update vo_1001_viz.R

--- sandbox/vo_1001_viz.R
+++ sandbox/vo_1001_viz.R
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 library(terra)
 
 #Get path to mosaic data
@@ -12,3 +13,19 @@ plot(metrics)
 #Aspect calculation and visualization
 aspect <- terrain(r1, v = "aspect")
 plot(aspect)
+=======
+library(terra)
+
+#Get path to mosaic data
+mosaic_dem <- "G:\\Y3S1\\GEO 871 Seminar in Physical Geography\\Git_R\\geodiversity_2025-main\\processed_tifs\\ORNL_2018_DEM_mosaic_20250925.tif"
+
+r1 <- rast(mosaic_dem)
+plot(r1)
+
+metrics <- terrain(r1)
+plot(metrics)
+
+#Aspect calculation and visualization
+aspect <- terrain(r1, v = "aspect")
+plot(aspect)
+>>>>>>> Stashed changes
