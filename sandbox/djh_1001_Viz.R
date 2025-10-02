cbd7ed36d6adf8956fc623f37e216ff329217cf3
DillonJHaller <hallerdi@msu.edu>
2025-10-02 08:32:28 -0400

    First tests of several geodiv methods

--- sandbox/djh_1001_Viz.R
+++ sandbox/djh_1001_Viz.R
@@ -1,4 +1,5 @@
 library(terra)
+library(geodiv)
 
 #Get path to mosaic data
 mosaic_dem <- "C:\\Users\\dillo\\Documents\\GitHub\\geodiversity_2025\\processed_tifs\\ORNL_2018_DEM_mosaic_20250925.tif"
@@ -6,8 +7,22 @@ mosaic_dem <- "C:\\Users\\dillo\\Documents\\GitHub\\geodiversity_2025\\processed
 r1 <- rast(mosaic_dem)
 plot(r1)
 
-metrics <- terrain(r1)
-plot(metrics)
+#Normalize the raster
+stats <- global(r1,fun = c("mean", "sd"))
+mu <- stats$mean
+sigma <- stats$sd
+normr <- (r1-mu)/sigma
 
-aspect <- terrain(r1, v = "aspect")
-plot(aspect)
+#Fractal dimension is a complex metric that measures the degree to which an image looks similar at all scales
+#This returns 2 regardless of if the raster is normalized
+frac <- sfd(r1)
+frac_n <- sfd(normr)
+
+#Radial wavelength, returns three values: the dominant wavelength, the dominance of that wavelength, and the wavelength to encompass half of values
+rad_dom <- srw(r1, create_plot = TRUE)
+rad_dom_n <-srw(normr)
+
+
+#Dominant texture direction? This may be an interesting one. Does it give us more than aspect?
+dir <- std(r1, create_plot = TRUE)
+dir_n <- std(normr)
