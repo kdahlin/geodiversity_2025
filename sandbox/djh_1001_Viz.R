30421ffafa887685220934b0750720326629116e
DillonJHaller <hallerdi@msu.edu>
2025-10-07 14:21:07 -0400

    Updates to geodiv metrics testing

--- sandbox/djh_1001_Viz.R
+++ sandbox/djh_1001_Viz.R
@@ -13,16 +13,24 @@ mu <- stats$mean
 sigma <- stats$sd
 normr <- (r1-mu)/sigma
 
+##In this section, I'm just going to test to see if normalizing the raster changes the results for any of these.
+
 #Fractal dimension is a complex metric that measures the degree to which an image looks similar at all scales
 #This returns 2 regardless of if the raster is normalized
 frac <- sfd(r1)
 frac_n <- sfd(normr)
-
 #Radial wavelength, returns three values: the dominant wavelength, the dominance of that wavelength, and the wavelength to encompass half of values
 rad_dom <- srw(r1, create_plot = TRUE)
 rad_dom_n <-srw(normr)
-
-
 #Dominant texture direction? This may be an interesting one. Does it give us more than aspect?
 dir <- std(r1, create_plot = TRUE)
 dir_n <- std(normr)
+#Texture aspect ratio (not the same as aspect)
+##THIS ONE DOES NOT STAY THE SAME AFTER NORMALIZATION
+asp_ratio <- stxr(r1)
+asp_ratio_n <- stxr(normr)
+#Correlation length. This is the minimum and maximum lengths to threshold autocorrelation
+#Thus these two numbers form the aspect ratio. This maximum seems not to be affected by normalization
+#But the minimum is strongly affected, which explains why the aspect ratio is.
+corrlen <- scl(r1, threshold = c(0.2))
+corrlen_n <- scl(normr, threshold = c(0.2))
