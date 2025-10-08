b917b83dce0e15515c81801c3f84b13c3b63a25a
fuesshar-gif <fuesshar@msu.edu>
2025-10-08 10:48:41 -0400

    Update hmf1001_viz.R

--- sandbox/hmf1001_viz.R
+++ sandbox/hmf1001_viz.R
@@ -1,11 +1,25 @@
 library(terra)
-library()
-## get path to mosaic:: 
+library(raster)
+library(tidyverse)
+library(geodiv)
 
-mosaic_dem <- "c:\\Users\\harryfuess\\Documents\\GitHub\\geodiversity_2025\\processed_tifs\\ORNL_2018_DEM_mosaic20250925.tif"
+## get path to mosaic:: 
+#note to self, use below code for creating
+mosaic_dem <-"processed_tifs/ORNL_2018_DEM_mosaic_20250925.tif"
 r1 <- rast(mosaic_dem)
 plot(r1)
 metrics <- terrain(r1)
 plot(metrics)
 
 aspect <- terrain (r1, v = "aspect")
+aspect
+
+#Oak Ridge (stripey) 
+# Prairie Potholes (lumpy) 
+# Central Plains (flat) 
+# Rocky Mountains (high relief) 
+# what are 
+
+
+
+
