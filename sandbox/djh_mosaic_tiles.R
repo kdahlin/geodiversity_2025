aede0a4eef6dbf4e5e7244a1ff24df921ee5fcd1
DillonJHaller <hallerdi@msu.edu>
2025-10-22 11:47:44 -0400

    Mosaiced RMNP files

---
+++ sandbox/djh_mosaic_tiles.R
@@ -0,0 +1,40 @@
+# -----
+# Step 7: Mosaic AOP tiles!
+# -----
+library(sf)
+library(terra)
+
+#Only part that should change between runs
+sitename = "RMNP"
+year = "2020" #Year of data acquisition, not current year
+
+
+date = toString(Sys.Date())
+
+NEON.path <- paste0("./NEON_data/",sitename,"/")
+
+dem.files <- list.files(NEON.path, pattern = "DSM.tif$", recursive = TRUE)
+
+bli = 33 #Index of the tile that forms the bottom left of the mosaic (bottom-left index)
+
+tile1 <- rast(paste0(NEON.path, dem.files[bli]))
+tile2 <- rast(paste0(NEON.path, dem.files[bli+1]))
+tile3 <- rast(paste0(NEON.path, dem.files[bli+7]))
+tile4 <- rast(paste0(NEON.path, dem.files[bli+8]))
+
+mosaic.dem <- mosaic(tile1, tile2, tile3, tile4)
+
+plot(mosaic.dem) # so pretty!
+
+#Make output directory
+dir.create(paste0("./processed_tifs/", sitename))
+out.mosaic.name <- paste0("./processed_tifs/", sitename, "/",
+                          sitename, "_", year, "_DEM_mosaic_",
+                          date, "_9.tif")
+
+writeRaster(mosaic.dem, out.mosaic.name,
+            overwrite = FALSE)
+
+# -----
+# all done!
+# -----
