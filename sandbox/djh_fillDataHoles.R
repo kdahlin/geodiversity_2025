216da7ae8df18770d3ea984b316d155ab7fd67c4
DillonJHaller <hallerdi@msu.edu>
2025-10-29 12:01:21 -0400

    Filled holes in rasters

---
+++ sandbox/djh_fillDataHoles.R
@@ -0,0 +1,27 @@
+library(terra)
+library(tools)
+
+#setwd(C:/...)#Uncomment this line if needed to set working directory to repo
+
+#Define NEON site location codes (subdirectory names)
+locations <- c("ORNL",
+               "RMNP",
+               "CPER",
+               "WOOD")
+
+#iterate over each NEON site  as a subdir of processed_tifs to get tif files
+tifs <- c()
+for (loc in locations) {
+  dir_path <- file.path("processed_tifs", loc)
+  tif_files <- list.files(path = dir_path, pattern = "\\d.tif$", full.names = TRUE)
+  tifs <- c(tifs, tif_files)
+}
+#Iterate over tifs
+for (tif in tifs){
+  r1 <- rast(tif)
+  #Apply a focal average of a 3x3 window to any NA values
+  f1 <- focal(r1, w=3, fun = "mean", na.policy = "only", na.rm=T)
+  #Wouldn't normally recommend this, but I am overwriting the original mosaics to
+  #save space
+  writeRaster(f1, tif, overwrite = T)
+}
