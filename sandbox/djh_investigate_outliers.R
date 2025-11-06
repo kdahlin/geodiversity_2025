1dd300e5830800c890707b544384204af555f0ee
DillonJHaller <hallerdi@msu.edu>
2025-11-05 21:23:41 -0500

    Added charts of which scenes are outliers in which metrics

--- sandbox/djh_investigate_outliers.R
+++ sandbox/djh_investigate_outliers.R
@@ -3,12 +3,13 @@ library(dplyr)
 library(ggplot2)
 library(cowplot)
 library(tidyr)
+
+setwd("C:/Users/hallerdi/Documents/Dillon_Classes/geodiversity_2025") 
+
 Metrics <- read_csv("sandbox/results/all_metrics_wide.csv")
-View(Metrics)
 n <- Metrics$func
 tpose <- as.data.frame(t(Metrics[,-1]))
 colnames(tpose) <- n
-View(tpose)
 
 #Calculate IQR per column
 IQRs <- sapply(tpose, IQR)
@@ -26,3 +27,16 @@ out_of_bounds <- Map(function(vals, lo, hi) {
 out_of_bounds
 
 counts <- table(unlist(out_of_bounds))
+
+#Make new dataframe which merely holds a 1 or a zero indicating whether the value is an outlier
+flags <- as.data.frame(
+  Map(function(vals, lo, hi) {
+    as.integer(vals < lo | vals > hi)
+  }, tpose, lower_threshold, upper_threshold)
+)
+rownames(flags) <- rownames(tpose)
+flags
+
+#Save out csvs
+write.csv(tpose, "sandbox/results/all_metrics_transposed.csv", row.names = TRUE)
+write.csv(flags, "sandbox/results/outlier_flags.csv", row.names = TRUE)
