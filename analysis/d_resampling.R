30e88aa76f9e4ccb8092965e3c7b6a62ac88e796
leobaldiga <leobaldiga@gmail.com>
2025-11-12 11:30:41 -0500

    new resampled rasters and resampling script

--- analysis/d_resampling.R
+++ analysis/d_resampling.R
@@ -31,78 +31,83 @@ target_resolutions <- c(5, 10, 15, 30, 50, 100)
 tifs <- c()
 for (loc in locations) {
   dir_path <- file.path("processed_tifs", loc)
-  tif_files <- list.files(path = dir_path, pattern = "\\.tif$", full.names = TRUE)
+  tif_files <- list.files(path = dir_path, pattern = "\\_1m.tif$", full.names = TRUE)
   tifs <- c(tifs, tif_files)
+}
+
+# Function to aggregate raster to each target resolution
+resample_and_save <- function(input_tif, resolutions) {
+  r <- rast(input_tif)
+  input_dir <- dirname(input_tif)
+  file_base <- sub("_1m$", "", sub("\\.tif$", "", basename(input_tif), ignore.case = TRUE))
+  
+  # Get original resolution (assumes square pixels)
+  orig_res <- res(r)[1]
+  
+  for (res in resolutions) {
+    # Calculate aggregation factor
+    agg_factor <- res / orig_res
+    
+    # Only use integer factors; otherwise, skip or warn about rounding
+    if (abs(round(agg_factor) - agg_factor) > .Machine$double.eps^0.5) {
+      warning(sprintf("Target res %s not an integer multiple of original (%s). Skipping.", res, orig_res))
+      next
+    }
+    agg_factor <- round(agg_factor)
+    
+    # Aggregate raster
+    r_agg <- aggregate(r, fact = agg_factor, fun = mean, na.rm = TRUE)
+    
+    # Output filename:
+    out_file <- file.path(input_dir, paste0(file_base, "_", res, "m.tif"))
+    writeRaster(r_agg, filename = out_file, overwrite = TRUE)
+    message("Saved aggregated raster to: ", out_file)
+  }
 }
 
 #--------------------------
 # RESAMPLING RASTERS
 #--------------------------
 
-# Function to resample and save rasters at multiple resolutions
-resample_and_save <- function(input_tif, site_code, resolutions, base_dir = "processed_tifs") {
-  # Load input raster
-  r <- rast(input_tif)
-  
-  for (res in resolutions) {
-    # Compute new resolution in x and y 
-    new_res <- c(res, res)
-    
-    # Resample raster to new resolution using bilinear interpolation (3x3 window)
-    r_resampled <- resample(r, rast(ext(r), resolution=new_res, crs=crs(r)), method = "bilinear")
-    
-    # Create output directory if it doesn't exist
-    out_dir <- file.path(base_dir, site_code, paste0("resampled_", res, "m"))
-    if (!dir.exists(out_dir)) {
-      dir.create(out_dir, recursive = TRUE)
-    }
-    
-    # Define output file path
-    file_base <- sub("\\.tif$", "", basename(input_tif), ignore.case = TRUE)
-    out_file <- file.path(out_dir, paste0(file_base, "_", res, "m.tif"))
-    
-    # Write resampled raster
-    writeRaster(r_resampled, filename = out_file, overwrite = TRUE)
-    
-    message("Saved resampled raster to: ", out_file)
-  }
-}
-
-# Loop over all files and sites
+# Loop over files in all locations
 for (loc in locations) {
   dir_path <- file.path("processed_tifs", loc)
-  tif_files <- list.files(path = dir_path, pattern = "\\.tif$", full.names = TRUE)
-  
+  tif_files <- list.files(path = dir_path, pattern = "_1m\\.tif$", full.names = TRUE)
   for (tif in tif_files) {
-    resample_and_save(tif, loc, target_resolutions)
+    resample_and_save(tif, target_resolutions)
   }
 }
 
+#--------------------------
+# PLOT ONE EXAMPLE RASTER AND CHECK RESOLUTIONS
+#--------------------------
 
-#load the first resampled raster from each site and each resolution to verify
-for (loc in locations) {
-  for (res in target_resolutions) {
-    resampled_dir <- file.path("processed_tifs", loc, paste0("resampled_", res, "m"))
-    resampled_files <- list.files(path = resampled_dir, pattern = "\\.tif$", full.names = TRUE)
-    
-    if (length(resampled_files) > 0) {
-      r_test <- rast(resampled_files[1])
-      print(paste("Site:", loc, "Resolution:", res, "m"))
-      print(r_test)
-    }
-  }
-}
+# Example: Plot original and resampled rasters for the first file
+example_tif <- tif_files[1]
+print(example_tif)
 
-#plot example raster - panel of different resolutions for one site
-example_site <- locations[1]
-par(mfrow=c(2,3))
+r_list <- list()
+
+# Load original raster (1m)
+r_list[["1m"]] <- rast(example_tif)
+
+# Load each resampled raster
 for (res in target_resolutions) {
-  resampled_dir <- file.path("processed_tifs", example_site, paste0("resampled_", res, "m"))
-  resampled_files <- list.files(path = resampled_dir, pattern = "\\.tif$", full.names = TRUE)
-  if (length(resampled_files) > 0) {
-    r_plot <- rast(resampled_files[1])
-    plot(r_plot, main=paste(example_site, "-", res, "m"))
-  }
+  resampled_file <- sub("_1m\\.tif$", paste0("_", res, "m.tif"), example_tif)
+  r_list[[paste0(res, "m")]] <- rast(resampled_file)
 }
 
-# End of script
+# Plot all: original first, then resampled
+par(mfrow = c(2, 4))
+plot(r_list[["1m"]], main = "Original 1m")
+for (res in target_resolutions) {
+  plot(r_list[[paste0(res, "m")]], main = paste0(res, "m"))
+}
+
+# Print info for each raster
+print(r_list[["1m"]])
+for (res in target_resolutions) {
+  print(r_list[[paste0(res, "m")]])
+}
+
+# End of script!
