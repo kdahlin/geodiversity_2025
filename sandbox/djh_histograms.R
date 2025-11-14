fd5839b9e5ac8576a09d265831b439f49557c6f7
DillonJHaller <hallerdi@msu.edu>
2025-11-14 18:10:06 -0500

    Added saving out plots without pushing them to repo

--- sandbox/djh_histograms.R
+++ sandbox/djh_histograms.R
@@ -1,6 +1,7 @@
 #This script generates histograms of each mosaic's heights and slopes
 library(terra)
 library(tools)
+library(ggplot2)
 
 #setwd(C:/...)#Uncomment this line if needed to set working directory to repo
 
@@ -20,8 +21,24 @@ for (loc in locations) {
 
 #Iterate over tifs
 for (tif in tifs) {
+  tifname <- strsplit(tif, "/")[[1]][3]
+  tifname <- strsplit(tifname, "\\.")[[1]][1]
+  
+  #Plots for elevation
   r1 <- rast(tif)
-  p1 <- plot(r1, pax = list(labels = FALSE, tick = FALSE))
+  png(paste0("sandbox/results/", tifname, "_elev_map.png"))
+  rp1 <- plot(r1, pax = list(labels = FALSE, tick = FALSE))
+  dev.off()
+  png(paste0("sandbox/results/", tifname, "_elev_hist.png"))
   rh1 <- hist(r1, maxcell = 4000000)
+  dev.off()
+  
+  #Plots for slope
   s1 <- terrain(r1)
+  png(paste0("sandbox/results/", tifname, "_slope_map.png"))
+  sp1 <- plot(s1, pax = list(labels = FALSE, tick = FALSE))
+  dev.off()
+  png(paste0("sandbox/results/", tifname, "_slope_hist.png"))
+  sh1 <- hist(s1, maxcell = 4000000)
+  dev.off()
 }
