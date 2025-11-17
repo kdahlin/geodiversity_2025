827232e08459566aa51fd66d2c94dc316c45a4d6
DillonJHaller <hallerdi@msu.edu>
2025-11-17 16:04:57 -0500

    Added a bit of documentation to histogram and map script

--- sandbox/djh_histograms.R
+++ sandbox/djh_histograms.R
@@ -1,4 +1,5 @@
-#This script generates histograms of each mosaic's heights and slopes
+#This script generates histograms and maps of each mosaic's heights and slopes
+#These will be saved locally in your sandbox/results folder but they don't upload to the repo
 library(terra)
 library(tools)
 library(ggplot2)
@@ -21,14 +22,14 @@ for (loc in locations) {
 
 #Iterate over tifs
 for (tif in tifs) {
-  tifname <- strsplit(tif, "/")[[1]][3]
-  tifname <- strsplit(tifname, "\\.")[[1]][1]
+  tifname <- strsplit(tif, "/")[[1]][3] #Remove folders of storage from the name
+  tifname <- strsplit(tifname, "\\.")[[1]][1] #Remove file extension
   
   #Plots for elevation
   r1 <- rast(tif)
-  png(paste0("sandbox/results/", tifname, "_elev_map.png"))
+  png(paste0("sandbox/results/", tifname, "_elev_map.png")) #Open a png, plot() output will be automatically written here
   rp1 <- plot(r1, pax = list(labels = FALSE, tick = FALSE))
-  dev.off()
+  dev.off() #Close the png
   png(paste0("sandbox/results/", tifname, "_elev_hist.png"))
   rh1 <- hist(r1, maxcell = 4000000)
   dev.off()
