723e75a44025695b9346a8024b1571f4aa00205b
kdahlin <kyla.dahlin@gmail.com>
2025-12-02 18:34:07 -0500

    messing with clustering

--- sandbox/kmd_cluster_analysis.R
+++ sandbox/kmd_cluster_analysis.R
@@ -45,19 +45,64 @@ row.names(in.data.t) <- rnames.new[,1]
 in.data.1m <- subset(in.data.t, res == "1m")
 
 ################################################################################
+### data exploration at 1 m resolution 
+
+# get just 1m resolution
+in.data.1m <- subset(in.data.t, res == "1m")
+
+# look at column variances (to remove columns with no variance)
+variances <- sapply(in.data.1m, var)
+
+# get the column names with no variance
+which.var <- which(variances == 0)
+
+# remove columns with no variance
+in.data.1m <- in.data.1m[,-(which.var)]
+
+### OPTIONAL - REMOVE HIGHLY CORRELATED VARIABLES AND WEIRD ONES
+remove.some <- c("tri_1", "tpi_1", "sq_1", "sa_1", "s10z_1", "sdq6_1", "sph_1", 
+                 "svi_1", "scl_1", "TRI_1", "TRIriley_1", "TRIrmsd_1", 
+                 "roughness_1", "std_1")
+
+remove.cols <- which(names(in.data.1m) %in% remove.some)
+
+small.data.1m <- in.data.1m[,-c(remove.cols)]
+
+################################################################################
 # clusters on subsites as rows
-cl <- hdbscan(in.data.1m, minPts = 4)
+cl <- hdbscan(small.data.1m, minPts = 3)
 
 print(cl)
 
 plot(cl, show_flat = TRUE)
 
 out.clusters <- cbind(rownames(in.data.1m), cl$cluster)
-
-# clusters on metrics as rows (all data?)
-
-cl.metrics <- hdbscan(in.data, minPts = 4)
+plot(small.data.1m$range_1, small.data.1m$curvature_total_1,
+     col = cl$cluster+1)
 
 
+# clusters on metrics as rows - transpose (again!)
+small.data.t <- as.data.frame(t(small.data.1m))
+
+cl.metrics <- hdbscan(small.data.t, minPts = 2)
+print(cl.metrics)
+plot(cl.metrics, show_flat = TRUE)
+metric.clusters <- cbind(rownames(small.data.t), cl.metrics$cluster)
+plot(small.data.1m$range_1, small.data.1m$curvature_total_1,
+     col = cl$cluster+1)
+
+######################### 
+# cluster on PCA metrics - not sure if this is bananas.
+
+pca <- prcomp(small.data.t, center = TRUE, scale = TRUE)
+# get the scores/PCs
+scores <- as.data.frame(summary(pca)$x)
+row.names(scores) <- row.names(small.data.t)
+
+cl.pcs <- hdbscan(scores, minPts = 3)
+print(cl.pcs)
+pc.clusters <- cbind(rownames(scores), cl.pcs$cluster)
+plot(scores[,c(1,2)], type = "n")
+text(scores[,c(1,2)], label = row.names(scores), col = cl.pcs$cluster+1)
 
 
