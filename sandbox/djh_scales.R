709bed16544c0b03cb56d35c6b26af27893bf889
DillonJHaller <hallerdi@msu.edu>
2025-12-04 10:53:46 -0500

    More scale figures

--- sandbox/djh_scales.R
+++ sandbox/djh_scales.R
@@ -4,8 +4,10 @@ library(openxlsx2)
 library(stringr) #Allows python-style string formatting
 library(dplyr)
 
+#####################################################################
 #Set working directory
 setwd("C:/Users/hallerdi/Documents/Dillon_Classes/geodiversity_2025")
+#####################################################################
 
 # read in the long data
 in.data <- read.csv("./results/all_metrics_long.csv")
@@ -29,7 +31,18 @@ norm.data <- in.data %>%
   ) %>%
   ungroup()
 
-#Iterate over variables
+#Different form of normalization. We normalize between all scenes at the same scale for the same function
+scale.norm.data <- in.data %>%
+  group_by(res, func) %>%
+  mutate(
+    max = max(value),
+    min = min(value),
+    norm_value = (value - min) / (max - min)
+  ) %>%
+  ungroup() 
+
+
+#Iterate over variables, saving out a raw value and a normalized value for each metric
 for (variable in unique_variables){
   #Subset data to only that currently of interest
   subset <- norm.data[norm.data$func==variable,]
@@ -41,5 +54,65 @@ for (variable in unique_variables){
     geom_line(mapping = aes(x = res, y = value, color = site, group = scene))+
     labs(x = "Pixel size (meters)", y = "Value", title = variable)
   ggsave(str_glue("./sandbox/results/scales/{variable}_rawvalue.png"), width = 5, height = 3.5, units = "in", limitsize = FALSE)
+  subset <- scale.norm.data[scale.norm.data$func == variable,]
+  ggplot(data = subset)+
+    geom_line(mapping = aes(x = res, y = norm_value, color = site, group = scene))+
+    labs(x = "Pixel size (meters)", y = "Relative value", title = variable)
+  ggsave(str_glue("./sandbox/results/scales/{variable}_scale_normalized.png"), width = 5, height = 3.5, units = "in", limitsize = FALSE)
 }
 
+####################
+#Summarized per-site
+####################
+
+#Summarize by mean, min and max for each site
+grouped.data <- in.data %>% group_by(func, site, res) %>% 
+  summarise(mean = mean(value, na.rm = TRUE)) %>%
+  mutate(res = as.numeric(res))
+norm.grouped.data <- grouped.data %>%
+  group_by(res, func) %>%
+  mutate(
+    max = max(mean),
+    min = min(mean),
+    norm_value = (mean - min) / (max - min)
+  ) %>%
+  ungroup() 
+
+
+#Massive plot of all metrics
+ggplot(grouped.data, aes(x = res, y = mean, color = site, group=site)) +
+  geom_point() +
+  geom_line() +
+  #geom_errorbar(aes(x = res, ymin = min, ymax = max), width = .2)+
+  facet_wrap(~ func, scales = "free_y") +
+  scale_x_continuous(breaks = sort(unique(grouped.data$res)))+
+  theme_bw() +
+  scale_color_manual(values = c("CPER" = "aquamarine4", "OAES" = "aquamarine4", "CLBJ" = "aquamarine4", 
+                                "ORNL" = "royalblue4", "MLBS" = "royalblue4", 
+                                "RMNP"="purple3", "TEAK"="purple3", "WREF"="purple3", 
+                                "WOOD"="darkorange3", "OSBS"="darkorange3", "UNDE"="darkorange3"))+
+  labs(x = "", y = "", title = "Mean change over resolutions") +
+  theme(title = element_text(size=20),
+        legend.text = element_text(size=15),
+        legend.title = element_text(size=20),
+        axis.text = element_text(size = 15),
+        strip.text = element_text(size = 20))
+ggsave("./sandbox/results/scales/MeanChangeResolutions.png", width = 70 , height = 40 , units = "cm", bg = "white")
+ggplot(norm.grouped.data, aes(x = res, y = norm_value, color = site, group=site)) +
+  geom_point() +
+  geom_line() +
+  #geom_errorbar(aes(x = res, ymin = min, ymax = max), width = .2)+
+  facet_wrap(~ func, scales = "free_y") +
+  scale_x_continuous(breaks = sort(unique(norm.grouped.data$res)))+
+  theme_bw() +
+  scale_color_manual(values = c("CPER" = "aquamarine4", "OAES" = "aquamarine4", "CLBJ" = "aquamarine4", 
+                                "ORNL" = "royalblue4", "MLBS" = "royalblue4", 
+                                "RMNP"="purple3", "TEAK"="purple3", "WREF"="purple3", 
+                                "WOOD"="darkorange3", "OSBS"="darkorange3", "UNDE"="darkorange3"))+
+  labs(x = "", y = "", title = "Relative mean change over resolutions") +
+  theme(title = element_text(size=20),
+        legend.text = element_text(size=15),
+        legend.title = element_text(size=20),
+        axis.text = element_text(size = 15),
+        strip.text = element_text(size = 20))
+ggsave("./sandbox/results/scales/NormMeanChangeResolutions.png", width = 70 , height = 40 , units = "cm", bg = "white")
