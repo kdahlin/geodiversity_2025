c6cb1c89971089f249a9ded158ef51f77608f219
Kyla Dahlin <kyla.dahlin@gmail.com>
2026-02-23 10:12:17 -0500

    finalizing data cleaning and organizing

--- analysis/e_corrplot_and_beeswarms.R
+++ analysis/e_corrplot_and_beeswarms.R
@@ -1,6 +1,7 @@
 ######Plotting######################
 library(readr)
 library(dplyr)
+library(corrplot)
 library(ggplot2)
 library(cowplot)
 library(tidyr)
@@ -12,19 +13,35 @@ Metrics.wide <- read.csv("./results/all_metrics_wide.csv")
 Metrics.long <- read.csv("./results/all_metrics_long.csv") 
 
 # rename to match manuscript & store
-Metrics$func <- c("Sa", "Sq", "s10z", "Sdq", "Sdq6", "Sdr", "SBI", "SCI", "Ssk",
-                  "Sku", "Sds", "Sfd", "Srw1", "Srw2", "Srw3", "Std1", "Std2", 
-                  "Svi", "Stxr1", "Stxr2", "Ssc", "Sv", "Sph", "Sk", "Smean", 
-                  "Spk", "Svk", "Scl1", "Scl2", "Sdc","TPIv1", "TRIv1", "VRM", 
-                  "SAR", "rEnt", "Slope", "nor", "east", "TPIv2", "TRIv2", 
-                  "TRIriley", "TRIrmsd", "rough", "stdv", "MAD", "nmodes", 
-                  "range", "CuPro", "CuPl", "CuTot")
 
-write.csv(Metrics[,-1], "./results/all_metrics_wide.csv", row.names = Metrics$func)
+old.names <- unique(Metrics.long$func)
+
+clean.names <- c("Sa", "Sq", "s10z", "Sdq", "Sdq6", 
+                  "Sdr", "SBI", "SCI", "Ssk", "Sku", 
+                  "Sds", "Sfd", "Srw1", "Srw2", "Srw3", 
+                  "Std1", "Std2", "Svi", "Stxr1", "Stxr2", 
+                  "Ssc", "Sv", "Sph", "Sk", "Smean", 
+                  "Spk", "Svk", "Scl1", "Scl2", "Sdc",
+                  "TPIv1", "TRIv1", "VRM", "SAR", "rEnt", 
+                  "Slope", "nor", "east", "TPIv2", "TRIv2", 
+                  "TRIriley", "TRIrmsd", "rough", "stdv", "MAD", 
+                  "nmodes", "range", "CuPro", "CuPl", "CuTot")
+
+
+for (i in 1:length(old.names)) {
+  Metrics.long$func[Metrics.long$func == old.names[i]] <- clean.names[i]
+}
+
+write.csv(Metrics.long, "./results/all_metrics_long_clean.csv")
+
+Metrics.wide$func <- clean.names
+
+write.csv(Metrics.wide[,-1], "./results/all_metrics_wide_clean.csv",
+          row.names = Metrics.wide$func)
 
 ################################################################################
-### data exploration at 1 m resolution 
-in.data <- read.csv("./results/all_metrics_wide.csv", row.names = 1) 
+### data exploration at 1 m resolution - this is a clunky way to do this...
+in.data <- read.csv("./results/all_metrics_wide_clean.csv", row.names = 1) 
 
 # transpose data so each row is a subsite and each column is a metric
 in.data.t <- as.data.frame(t(in.data))
@@ -70,7 +87,7 @@ in.data.1m <- in.data.1m[,-(which.var)]
 # look at correlations
 cors <- cor(in.data.1m)
 
-#write.csv(cors, "./results/correlation_matrix_1m.csv", row.names = TRUE)
+write.csv(cors, "./results/correlation_matrix_1m.csv", row.names = TRUE)
 
 png(filename = "./results/correlation_matrix.png", 
     width = 6, height = 6, units = "in", res = 300, bg = "white")
@@ -79,54 +96,37 @@ corrplot(cors, order = "FPC", method = "circle", tl.cex = 0.6)
 dev.off()
 
 ########## Remove highly correlated ones ###########################
+in.data.long <- read.csv("./results/all_metrics_long_clean.csv") 
 
-remove.metrics <- c("Sa", "Sq", "s10z", "Sdq6", "Sph", "Svi", "Scl1",
+remove.metrics <- c("Sa", "Sq", "s10z", "Sdq6", "Sph", "Svi", "Sv", "Scl1",
                     "TRIv1", "TRIriley", "TRIrmsd", "rough",
-                    "Std1", "TPIv1", "TPIv1", "TRIv2", "srw1", "Scl2")
+                    "Std1", "TPIv2", "TRIv2", "Srw1", "Scl2")
 
-check <- !(Metrics$func %in% remove.metrics)
-Metrics.sub <- filter(Metrics, check)
-
-#Pivot data to format for plotting
-Metrics_Long<- Metrics.sub %>%
-  mutate(func = gsub("_.*", "", func)) %>%  # remove anything after the first "_" from func column
-  pivot_longer(
-    cols = -func,               # except first row
-    names_to = "Raster",        # new column for raster
-    values_to = "value"         # new column for the metric value
-  ) %>%
-  rename(metrics = func) %>%       # rename func to metrics
-  select(metrics, value, Raster) %>%
-  mutate(raster_id = substr(Raster, 1, 4),   # add new column with locations, ID first 4 letters in raster
-         resolution = sub(".*_(\\d+)m\\.tif$", "\\1", Raster),     # extract number before "m.tif" 
-         Tile = sub(".*_(\\d+)_\\d+m\\.tif$", "\\1", Raster)) %>%  # second-to-last number   
-  drop_na()  #remove NA values
+check <- !(in.data.long$func %in% remove.metrics)
+Metrics.sub <- filter(in.data.long, check)
 
 #filter resolutions
-Metrics_LongRes <- Metrics_Long %>% filter(resolution==1)
+Metrics_LongRes <- Metrics.sub %>% filter(res==1)
 
 ###Plot beeswarm for all variables
 Metrics_LongRes_all <- Metrics_LongRes %>%
-  mutate(raster_id = factor(raster_id, levels = c("CPER", "OAES", "CLBJ",
+  mutate(site = factor(site, levels = c("CPER", "OAES", "CLBJ",
                                                   "WOOD", "OSBS","UNDE",
                                                   "ORNL", "MLBS",  "RMNP", 
                                                   "TEAK", "WREF")),
-         metrics = factor(metrics,
-                          levels = c("range", "smean", "svk", "spk", "mad",
-                                     "slope", "northness", "eastness", "sdq",
-                                     "sdr", "sar",
-                                     "sbi", "sci", "ssk", "sku", "sds", "sv",
-                                     "sdc", "nmodes",
-                                     "ssc", "Profile Curvature", "Planform Curvature",
-                                     "Total Curvature",
-                                     "sk", "stdv", "vrm", "tpi",
-                                     "sfd", "srw2", "srw3", "raster.entropy", 
-                                     "std", "stxr1", "stxr2")
-         ))
+         func = factor(func,
+                          levels = c("range", "MAD", "Smean", "Spk", "Svk",
+                                     "Slope", "nor", "east", "SAR", "Sdq", "Sdr",
+                                     "nmodes", "SBI", "SCI", "Ssk", "Sku", "Sds", 
+                                     "Sdc", "CuPro", "CuPl", "CuTot", "Ssc", 
+                                     "TPIv1", "stdv", "VRM", "rEnt", "Sk", "Sfd", 
+                                     "Srw2", "Srw3", "Std2", "Stxr1", "Stxr2")),
+         tile = factor(tile, levels = as.character(1:9))
+         )
 
-x <- ggplot(Metrics_LongRes_all, aes(x = raster_id, y = value, color = raster_id, shape = Tile)) +
+x <- ggplot(Metrics_LongRes_all, aes(x = site, y = value, color = site, shape = tile)) +
   geom_beeswarm(size=1) +
-  facet_wrap(~ metrics, scales = "free_y", ncol=4) +  # metrics by type
+  facet_wrap(~ func, scales = "free_y", ncol=4) +  # metrics by type
   theme_bw() +
   scale_color_manual(values = c("CPER" = "#a6cee3", "OAES" = "#a6cee3", "CLBJ" = "#a6cee3", 
                                 "ORNL" = "#b2df8a", "MLBS" = "#b2df8a", 
@@ -140,9 +140,9 @@ x <- ggplot(Metrics_LongRes_all, aes(x = raster_id, y = value, color = raster_id
     axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size = 6),
     axis.text.y = element_text(size = 5)
   ) +
-  theme(legend.position = "none")
+  theme(legend.position = "bottom")
 
 tag_facet(x, tag_pool = c(letters, LETTERS), size = 3)
 
-ggsave("./results/all_metrics_beewarm.png", width = 6.5 , height = 8 , units = "in", 
+ggsave("./results/all_metrics_beewarm.png", width = 6.5 , height = 9.5 , units = "in", 
        dpi = 300)
