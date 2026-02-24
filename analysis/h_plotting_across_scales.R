8dce84411314a35151c349731858c56d8663bd13
Kyla Dahlin <kyla.dahlin@gmail.com>
2026-02-24 12:06:35 -0500

    some more plotting for discussion

--- analysis/h_plotting_across_scales.R
+++ analysis/h_plotting_across_scales.R
@@ -47,3 +47,24 @@ ggplot(grouped.data, aes(x = res, y = mean, color = site, group=site)) +
         legend.key.height = unit(0.1, "in"))
 ggsave("./results/all_metrics_resolution.tif", width = 6.5 , height = 9.5, 
        units = "in", dpi = 300)
+
+# look at VRM only for discussion section writing
+vrm.only <- subset(grouped.data, grouped.data$func == "VRM")
+x11()
+ggplot(vrm.only, aes(x = res, y = mean, color = site, group=site)) +
+  geom_point() +
+  geom_line() +
+  #geom_errorbar(aes(x = res, ymin = min, ymax = max), width = .2)+
+  scale_x_continuous(breaks = sort(unique(grouped.data$res)))+
+  theme_bw() +
+  scale_color_manual(values = c("CPER" = "#a6cee3", "OAES" = "#a6cee3", "CLBJ" = "#a6cee3", 
+                                "ORNL" = "#b2df8a", "MLBS" = "#b2df8a", 
+                                "RMNP"="#33a02c", "TEAK"="#33a02c", "WREF"="#33a02c", 
+                                "WOOD"="#1f78b4", "OSBS"="#1f78b4", "UNDE"="#1f78b4"))+
+  labs(x = "", y = "", title = "") +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size = 6),
+    axis.text.y = element_text(size = 5)
+  ) +
+  theme(legend.position = "bottom", 
+        legend.key.height = unit(0.1, "in"))
